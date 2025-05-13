749b2f833dd2cd1819571197a5232334dd882da2
no-regret666 <RangelJara195@gmail.com>
2025-05-13 13:27:47 +0800

    add main.go

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	Server "nrMQ/server"
+
+	"github.com/cloudwego/kitex/server"
+)
+
 func main() {
 	//start the broker server
+	addr, _ := net.ResolveIPAddr("tcp", ":8888")
+	var opts []server.Option
+	opts = append(opts, server.WithServiceAddr(addr))
+	rpcServer := new(Server.RPCServer)
 
+	err := rpcServer.Start(opts)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
